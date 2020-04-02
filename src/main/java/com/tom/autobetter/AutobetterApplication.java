5ebf8ef3770fabf37ecc8f70b219c9ebbdf7626f
tomeyre <tom_1237@hotmail.com>
2020-04-02 21:50:11 +0100

    first commit

---
+++ src/main/java/com/tom/autobetter/AutobetterApplication.java
@@ -0,0 +1,13 @@
+package com.tom.autobetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobetterApplication.class, args);
+	}
+
+}
